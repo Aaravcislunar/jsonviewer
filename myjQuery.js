e7e4220244b01e81d971e997fd4207bb59b1fb69
Aarav Gupta <77160837+Aaravcislunar@users.noreply.github.com>
2023-05-26 12:01:42 +0530

    Add files via upload

--- myjQuery.js
+++ myjQuery.js
@@ -1,11 +1,24 @@
-
 $(document).ready(function () {
     $('#submit').on('click', function () {
-        var jsonData = JSON.parse(document.getElementById('jsoncode').value);
-        $('#json-renderer').jsonViewer(jsonData, { collapsed: true, withQuotes: false, withLinks: false });
+        var jsonData = document.getElementById('jsoncode').value;
+        console.log(jsonData);
+        function testJSON(text) {
+            try {
+                JSON.parse(text);
+                return true;
+            }
+            catch (e) {
+             
+                return false;
+            }
+        };
+        ;
+        if(testJSON(jsonData)){
+            $('#json-renderer').jsonViewer(JSON.parse(jsonData), { collapsed: true,
+                withQuotes: false, withLinks: false });
+        }
+        else{
+            $('#json-renderer').text('Invalid JSON');
+        }
     });
-
 });
-
-
-
